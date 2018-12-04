7c919f1986510b2e72d867d515c4b81f69d7422f
Adrien Lefrancq <irdane2111@gmail.com>
2018-12-03 19:46:57 -0600

    je sais pas ce que je vais faire de ma vie ...

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,11 +14,28 @@ function myTimer() {
   });
 }
 
-// document.querySelectorAll("a").forEach((a) => {
-//   a.addEventListener("click", function() {
-//     $("a:parent:parent")
-//     $.ajax({
-//     url: 'articles/marklu',
-//     });
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+const addflux = document.getElementById("addflux");
+addflux.addEventListener("click", (event) => {
+  event.currentTarget.classList.toggle("hide");
+  const addform = document.getElementById("addform");
+  addform.classList.toggle("hide");
+});
+
+// const clickread = document.querySelectorAll("a").forEach((a) => {
+//   a.addEventListener("click", (event) => {
+//     console.log("event");
+//     const link = event.currentTarget.nextSibling;
+//     console.log(link);
 //   });
-// });
+// });;
+
