4635d6f2acbdd9177a90746d0c7a4c4b85517d34
Adrien Lefrancq <irdane2111@gmail.com>
2018-12-04 12:07:34 -0600

    debug1

---
+++ app/javascript/components/Form.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Form extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        json.messages(@messages) do |message|
+          json.extract! message, :title, :title
+          json.url message_url(message, format: :json)
+        end
+      </React.Fragment>
+    );
+  }
+}
+
+Form.propTypes = {
+  greeting: PropTypes.string
+};
+export default Form
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,15 +14,15 @@ function myTimer() {
   });
 }
 
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
+// function eventFire(el, etype){
+//   if (el.fireEvent) {
+//     el.fireEvent('on' + etype);
+//   } else {
+//     var evObj = document.createEvent('Events');
+//     evObj.initEvent(etype, true, false);
+//     el.dispatchEvent(evObj);
+//   }
+// }
 
 const addflux = document.getElementById("addflux");
 addflux.addEventListener("click", (event) => {
