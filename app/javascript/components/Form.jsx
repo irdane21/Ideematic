9a772fb188a37db0a95c3336303a01ebb61579a7
Adrien Lefrancq <irdane2111@gmail.com>
2018-12-10 18:45:16 -0600

    BIG JUMP on form

--- app/javascript/components/Form.jsx
+++ app/javascript/components/Form.jsx
@@ -6,31 +6,45 @@ import axios from 'axios'
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value1: '', value2: '', open: false};
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleClick(){
+    this.setState({ open: !this.state.open })
   }
 
-  handleSubmit(evet){
+  handleChange1(event) {
+    this.setState({value1: event.target.value});
+  }
+
+  handleChange2(event) {
+    this.setState({value2: event.target.value});
+  }
+
+  handleSubmit(event){
     let message = event.target.value
     axios.post('/fluxes/create', `Title=${Title}`, `Url=${Url}`)
-
+    this.state({ open: false})
   }
   render () {
-    return (<div>
+    return (<div id="new-flux">
+      { !this.state.open && <div className="btn btn-success" onClick={this.handleClick}>New Flux</div>}
+      { this.state.open &&
         <form onSubmit={this.handleSubmit}>
           <p>Title</p>
-          <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange}/>
+          <input type="text" className="form-control" value={this.state.value1} onChange={this.handleChange1}/>
           <p>Url</p>
-          <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange}/>
-          <button className="btn btn-success" type="submit" remote="true">Create</button>
+          <input type="text" className="form-control" value={this.state.value2} onChange={this.handleChange2}/>
+          <button className="btn btn-success" type="submit">Create</button>
         </form>
-      </div>);
+      }
+      </div>
+    );
   }
 }
 
